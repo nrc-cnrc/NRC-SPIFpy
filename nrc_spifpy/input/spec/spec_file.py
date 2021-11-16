d6e95485079f035b33c2492e5e31aaaf3e174e63
Kenny Bala <kennybala97@outlook.com>
2021-11-16 14:46:48 -0500

    Checked writing of image data using new code

--- nrc_spifpy/input/spec/spec_file.py
+++ nrc_spifpy/input/spec/spec_file.py
@@ -5,8 +5,12 @@ import numpy as np
 from nrc_spifpy.input.binary_file import BinaryFile
 from nrc_spifpy.images import Images
 from nrc_spifpy.input.spec.buffer import Buffer
+from nrc_spifpy.input.spec.image import AssembledImageRecordContainer
+from nrc_spifpy.spif import TIME_CHUNK
 
 from tqdm import tqdm
+
+import joblib as jb
 
 file_buffer_template = np.dtype([
     ('year', 'u2'),
@@ -51,6 +55,7 @@ class SPECFile(BinaryFile):
         self.diodes = 128
         self.file_dtype = file_buffer_template
         self.aux_channels = aux_params
+        self.chunk_size = 250
 
 
     def calc_buffer_datetimes(self):
@@ -70,14 +75,23 @@ class SPECFile(BinaryFile):
     def process_file(self, spiffile, processors):
         spiffile.set_start_date(self.start_date.strftime('%Y-%m-%d %H:%M:%S %z'))
 
+        group_no_channel = self.name
+        group_h_channel = self.name + '-H'
+        group_v_channel = self.name + '-V'
+
         if self.name == '2DS':
-            spiffile.create_inst_group(self.name + '-V')
-            spiffile.create_inst_group(self.name + '-H')
-            spiffile.set_filenames_attr(self.name + '-V', self.filename)
-            spiffile.set_filenames_attr(self.name + '-H', self.filename)
+            spiffile.create_inst_group(group_h_channel)
+            spiffile.create_inst_group(group_v_channel)
+            spiffile.set_filenames_attr(group_h_channel, self.filename)
+            spiffile.set_filenames_attr(group_v_channel, self.filename)
+
+            self.add_auxiliary_core_variables(spiffile, group_h_channel)
+            self.add_auxiliary_core_variables(spiffile, group_v_channel)
         else:
-            spiffile.create_inst_group(self.name)
-            spiffile.set_filenames_attr(self.name, self.filename)
+            spiffile.create_inst_group(group_no_channel)
+            spiffile.set_filenames_attr(group_no_channel, self.filename)
+
+            self.add_auxiliary_core_variables(self, spiffile, group_no_channel)
 
         spiffile.write_buffer_info(self.start_date, self.datetimes)
 
@@ -85,37 +99,162 @@ class SPECFile(BinaryFile):
                      total=len(self.data),
                      unit='frame')
 
-        for i, buffer in enumerate(self.data):
+        chunk_intervals = self.get_chunk_intervals()
 
-            buffer = Buffer(i, self.timestamps[i], buffer['data'])
+        for i in range(len(chunk_intervals) - 1):
+            chunk_slice = slice(chunk_intervals[i], chunk_intervals[i + 1])
+            
+            buffer_ids = [x for x in range(chunk_intervals[i], chunk_intervals[i + 1])]
+            buffer_timestamps = self.timestamps[chunk_slice]
+            buffers = self.data[chunk_slice]['data']
 
-            #merged_buffers_h, merged_buffers_v = self.merge_buffers(buffer)
+            chunk_processor = ChunkedBufferProcessor(buffer_ids, buffer_timestamps, buffers)
+            chunk_processor.process_buffers()
+            images_h, images_v = chunk_processor.merge_assembled_images()
 
-            #self.write_data(spiffile, merged_buffers_h, merged_buffers_v)
-            #self.write_data(spiffile, buffer.image_containers['h'], buffer.image_containers['v'])
+            if self.name == '2DS':
+                self.write_data(spiffile, group_h_channel, images_h)
+                self.write_data(spiffile, group_v_channel, images_v)
+            else:
+                self.write_data(spiffile, group_no_channel, images_h)
 
-            pbar1.update(1)
+            pbar1.update(chunk_intervals[i + 1] - chunk_intervals[i])
+
+    def add_auxiliary_core_variables(self, spiffile, inst_name):
+        coregrp = spiffile.rootgrp[inst_name]['core']
+
+        spiffile.create_variable(
+            coregrp,
+            'num_words',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Number of data words present in image',
+                'units':'counts'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'timing_flag',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Timing flag for image',
+                'units':'boolean'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'mismatch_flag',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Mismatch flag for image',
+                'units':'boolean'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'fifo_flag',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'FIFO flag for image',
+                'units':'boolean'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'overload_flag',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Overload flag for image',
+                'units':'binary'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'particle_count',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Number of particles detected in image',
+                'units':'counts'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+        
+        spiffile.create_variable(
+            coregrp,
+            'num_slices',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Number of slices detected in the image',
+                'units':'counts'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'timeword_upper',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Upper 16 bits of timeword for image',
+                'units':'clock ticks'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'timeword_lower',
+            'u2',
+            ('Images',),
+            {
+                'long_name':'Lower 16 bits of timeword for image',
+                'units':'clock ticks'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
+
+        spiffile.create_variable(
+            coregrp,
+            'tas',
+            'f',
+            ('Images',),
+            {
+                'long_name':'True airspeed as recorded by probe',
+                'units':'m/s'
+            },
+            chunksizes=(TIME_CHUNK,)
+        )
 
     def get_chunk_intervals(self):
         data_len = len(self.data)
 
-        chunk_intervals = [i for i in range(data_len, 500)]
+        chunk_intervals = np.arange(0, data_len, self.chunk_size)
 
-        if chunk_intervals[-1] < data_len:
-            chunk_intervals.append(data_len)
+        if chunk_intervals[-1] != data_len:
+            chunk_intervals[-1] = data_len
+
         return chunk_intervals
 
-    def merge_buffers(self, buffers):
-        merged_buffers_h = Images(aux_params)
-        merged_buffers_v = Images(aux_params)
 
-        for buffer in buffers:
-            merged_buffers_h.extend(buffer.image_containers['h'])
-            merged_buffers_v.extend(buffer.image_containers['v'])
-
-        return merged_buffers_h, merged_buffers_v
-
-    def write_data(self, spiffile, h_p, v_p):
+    def write_data(self, spiffile, instrument, images):
         """ Called each time number of unsaved processed images exceeds
         10,000. Writes H and V images to SPIF NetCDF file.
         Parameters
@@ -128,14 +267,67 @@ class SPECFile(BinaryFile):
             Images object containing processed V array image info.
         """
 
-        if self.name == '2DS':
-            if len(h_p) > 0:
-                h_p.conv_to_array(self.diodes)
-                spiffile.write_images(self.name + '-H', h_p)
-            v_suffix = '-V'
-        else:
-            v_suffix = ''
+        instgrp = spiffile.rootgrp[instrument]
+        coregrp = spiffile.rootgrp[instrument]['core']
 
-        if len(v_p) > 0:
-            v_p.conv_to_array(self.diodes)
-            spiffile.write_images(self.name + v_suffix, v_p)
+        dim_size = len(instgrp.dimensions['Images'])
+        px_size = len(instgrp.dimensions['Pixels'])
+
+        # Primary, critical SPIF variables
+
+        spiffile.write_variable(coregrp, 'image_sec', images.image_sec, dim_size)
+        spiffile.write_variable(coregrp, 'image_ns', images.image_ns, dim_size)
+        spiffile.write_variable(coregrp, 'image_len', images.image_len, dim_size)
+        spiffile.write_variable(coregrp, 'buffer_index', images.buffer_id, dim_size)
+        spiffile.write_variable(coregrp, 'image', images.image, px_size)
+
+        # Auxiliary SPIF variables
+
+        spiffile.write_variable(coregrp, 'num_words', images.num_words, dim_size)
+        spiffile.write_variable(coregrp, 'timing_flag', images.timing_flag, dim_size)
+        spiffile.write_variable(coregrp, 'mismatch_flag', images.mismatch_flag, dim_size)
+        spiffile.write_variable(coregrp, 'fifo_flag', images.fifo_flag, dim_size)
+        spiffile.write_variable(coregrp, 'overload_flag', images.overload_flag, dim_size)
+        spiffile.write_variable(coregrp, 'particle_count', images.particle_count, dim_size)
+        spiffile.write_variable(coregrp, 'num_slices', images.num_slices, dim_size)
+        spiffile.write_variable(coregrp, 'timeword_upper', images.timeword_upper, dim_size)
+        spiffile.write_variable(coregrp, 'timeword_lower', images.timeword_lower, dim_size)
+        spiffile.write_variable(coregrp, 'tas', images.tas, dim_size)
+
+        spiffile.rootgrp.sync()
+
+class ChunkedBufferProcessor:
+
+    def __init__(self, buffer_ids, buffer_timestamps, buffers):
+        self.buffer_ids = buffer_ids
+        self.buffer_timestamps = buffer_timestamps
+        self.buffers = buffers
+
+        self.output = [None]*len(buffers)
+
+
+    def process_buffers(self):
+
+        for i in range(len(self.output)):
+            self.output[i] = self.process_buffer(
+                self.buffer_ids[i],
+                self.buffer_timestamps[i],
+                self.buffers[i]
+            )
+
+    def process_buffer(self, buffer_id, buffer_timestamp, buffer):
+
+        buffer = Buffer(buffer_id, buffer_timestamp, buffer)
+        return buffer.assembled_images
+
+    def merge_assembled_images(self):
+
+        image_container_h = AssembledImageRecordContainer()
+        image_container_v = AssembledImageRecordContainer()
+
+        for k in image_container_h.__dict__.keys():
+            image_container_h.__dict__[k] = np.concatenate([x['h'].__dict__[k] for x in self.output])
+            image_container_v.__dict__[k] = np.concatenate([x['v'].__dict__[k] for x in self.output])
+
+        return image_container_h, image_container_v
+
